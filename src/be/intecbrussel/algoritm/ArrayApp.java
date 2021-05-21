3514e3ea58a5f0554122f92ff3f11186207a3be3
Seunghointec <84398373+Seunghointec@users.noreply.github.com>
2021-05-21 15:11:13 +0200

    Opdract1

---
+++ src/be/intecbrussel/algoritm/ArrayApp.java
@@ -0,0 +1,30 @@
+package be.intecbrussel.algoritm;
+
+
+import java.util.Scanner;
+
+public class ArrayApp {
+    public static void main(String[] args) {
+        int[] numbers;
+
+        numbers = new int[]{7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 126, 133, 140};
+
+        for (int n: numbers) {
+            System.out.println(numbers[n]);
+        }
+    }
+}
+
+
+/*for (int i=0; i > numbers.length-1; i--) {
+        System.out.println(numbers[i]);
+        }*/
+
+
+
+
+  /*  Scanner keyboard =new Scanner(System.in);
+    String text = keyboard.nextLine();
+        for (int i=0; i < text.length(), )*/
+    
+
