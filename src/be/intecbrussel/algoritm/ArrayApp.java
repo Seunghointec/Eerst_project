b0b2ee882f385ab4155ed10f1ad2a3d30bf664d5
Seunghointec <84398373+Seunghointec@users.noreply.github.com>
2021-05-23 14:15:44 +0200

    Opdract1

--- src/be/intecbrussel/algoritm/ArrayApp.java
+++ src/be/intecbrussel/algoritm/ArrayApp.java
@@ -1,30 +1,43 @@
 package be.intecbrussel.algoritm;
 
 
-import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayApp {
+
     public static void main(String[] args) {
         int[] numbers;
 
         numbers = new int[]{7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105, 112, 119, 126, 133, 140};
 
         for (int n: numbers) {
-            System.out.println(numbers[n]);
+            System.out.println(n);
+        }
+        for (int n= numbers.length-1; n>=0; n--) {
+        System.out.println(numbers[n] + "");
+        }
+        //assignment 3
+        int [][] array2d = new int [4][6];
+        Random rand = new Random();
+            for(int row=0; row<4; row++) {
+                for(int el=0; el<6 ; el++) {
+                    array2d [row][el] = rand.nextInt(20);
+                    System.out.println(array2d[row][el] +"\t");
+                }
+                System.out.println("\n");
+            }
         }
     }
-}
-
-
-/*for (int i=0; i > numbers.length-1; i--) {
-        System.out.println(numbers[i]);
-        }*/
 
 
 
 
-  /*  Scanner keyboard =new Scanner(System.in);
-    String text = keyboard.nextLine();
-        for (int i=0; i < text.length(), )*/
+
+
+
+
+
     
 
